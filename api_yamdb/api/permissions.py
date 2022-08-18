1b4720531f8b7c80fdf10c9d15cd25e464e0bed5
Maxim Myasischev <mnmyasis@gmail.com>
2022-08-18 17:37:13 +0300

    feature(permissions): create permissions OnlyAdmin and AllowEditOrReadOnly
    - OnlyAdmin доступ к данным может получить только юзер с ролью admin.
    - AllowEditOrReadOnly редактировать могут владелец или юзеры с ролями
      moderator, admin. Остальные могут только читать.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,19 +1,31 @@
 from rest_framework import permissions
 
 
-class ContributionOrModeratorOrAdmin(permissions.BasePermission):
-    ALLOW_ROLE = [
-        'user',
+class AllowEditOrReadOnly(permissions.BasePermission):
+    """Редактировать могут только владелец, админ, модератор."""
+
+    ALLOW_EDIT_ROLE = [
         'moderator',
         'admin'
     ]
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.role in self.ALLOW_ROLE
-        return False
+        if request.method not in permissions.SAFE_METHODS:
+            return request.user.is_authenticated
+        return True
 
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.author
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user == obj.author
+            or request.user.role in self.ALLOW_EDIT_ROLE
+        )
+
+
+class OnlyAdmin(permissions.BasePermission):
+    """Только администратор."""
+    ALLOW_ROLE = 'admin'
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.role == self.ALLOW_ROLE
