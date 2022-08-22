21c36057ba4e994824f47a02911312e37affaf86
Maxim Myasischev <mnmyasis@gmail.com>
2022-08-22 08:34:40 +0300

    fix(user-model): change user model
    - Изменена реализация модели юзер.
    - Удален кастомный мененджер.
    - Внесены правки в permissions под новую модель.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,15 +1,5 @@
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
-)
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-from random import randint
-
-from django.core.mail import send_mail
-from rest_framework_simplejwt.tokens import RefreshToken
-
-from api_yamdb.settings import RECIPIENTS_EMAIL
 
 USER_ROLE = 'user'
 MODERATOR_ROLE = 'moderator'
@@ -21,88 +11,43 @@ ROLE_CHOICES = (
     (ADMIN_ROLE, 'Администратор')
 )
 
-
-class UserManager(BaseUserManager):
-    def create_user(self, **kwargs):
-        """ Создает и возвращает пользователя с имэйлом и именем. """
-        if kwargs.get('username') is None:
-            raise TypeError('Users must have a username.')
-
-        if kwargs.get('email') is None:
-            raise TypeError('Users must have an email address.')
-
-        random = randint(100000, 1000000)
-        kwargs['secret_key'] = random
-        user = self.model(**kwargs)
-        user.save()
-        send_mail(
-            'Регистрация нового пользователя',
-            'Это ваш token для получения JWTТокена:' f'{random}',
-            RECIPIENTS_EMAIL,
-            [user.email],
-        )
-        return user
-
-    def create_superuser(self, **kwargs):
-        """ Создает и возввращет пользователя с привилегиями суперадмина. """
-        if kwargs.get('password') is None:
-            raise TypeError('Superusers must have a password.')
-
-        user = self.create_user(**kwargs)
-        user.is_superuser = True
-        user.is_staff = True
-        user.role = ADMIN_ROLE
-        user.save()
-
-        return user
+MAX_LENGTH_USERNAME = 255
+MAX_LENGTH_CONFIRMATION_CODE = 256
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(db_index=True, max_length=255, unique=True)
-    email = models.EmailField(db_index=True, unique=True)
+def get_max_length_role(roles):
+    max_length = len(roles[1][0])
+    for i in range(1, len(roles)):
+        role_len = len(roles[i][0])
+        if role_len > max_length:
+            max_length = role_len
+    return max_length
 
-    first_name = models.CharField(max_length=256, blank=True, null=True)
-    last_name = models.CharField(max_length=256, blank=True, null=True)
 
-    secret_key = models.CharField(max_length=256,
-                                  db_index=True, unique=True, null=True)
-
-    is_active = models.BooleanField(default=True)
-
-    is_staff = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    updated_at = models.DateTimeField(auto_now=True)
-
+class User(AbstractUser):
+    confirmation_code = models.CharField(
+        max_length=MAX_LENGTH_CONFIRMATION_CODE,
+        db_index=True,
+        unique=True,
+        null=True
+    )
     bio = models.TextField(blank=True, null=True)
     role = models.CharField(
-        max_length=20,
+        max_length=get_max_length_role(ROLE_CHOICES),
         default=USER_ROLE,
         choices=ROLE_CHOICES
     )
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
-    objects = UserManager()
 
     def __str__(self):
         return self.email
 
     @property
-    def token(self):
-        return self.get_tokens_for_user()
+    def is_admin(self):
+        return self.is_staff or self.role == ADMIN_ROLE
 
-    def get_full_name(self):
-        return self.username
-
-    def get_short_name(self):
-        return self.username
-
-    def get_tokens_for_user(self):
-        refresh = RefreshToken.for_user(self)
-        return str(refresh.access_token)
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR_ROLE
 
     class Meta:
-        ordering = ['pk']
+        ordering = ['username']
